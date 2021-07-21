71e4e553253d1f95de5e15fca9b10f31f39dabdf
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-21 16:37:16 -0300

    Refatorado código e refeito função de coppy

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(() => {
+  const lTopInput = $("input#b1");
+  const rTopInput = $("input#b2");
+  const lBottomInput = $("input#b3");
+  const rBottomInput = $("input#b4");
 
-  const lTopInput = $('input#b1');
-  const rTopInput = $('input#b2');
-  const lBottomInput = $('input#b3');
-  const rBottomInput = $('input#b4');
-
-  const box = $('.box');
-  const preview = $('span#result');
-  const copyBtn = $('button#copy-result');
+  const box = $(".box");
+  const preview = $("span#result");
+  const copyBtn = $("button#copy-result");
 
   const regex = /([0-9])/g;
 
@@ -19,16 +18,35 @@ $(document).ready(() => {
   copyBtn.click(() => copyResults());
 
   function getValues() {
+    let rTValue = rTopInput.val().match(regex) || [""];
+    let rBValue = rBottomInput.val().match(regex) || [""];
+    let lTValue = lTopInput.val().match(regex) || [""];
+    let lBValue = lBottomInput.val().match(regex) || [""];
 
-    let rTValue = rTopInput.val().match(regex) || [''];
-    let rBValue = rBottomInput.val().match(regex) || [''];
-    let lTValue = lTopInput.val().match(regex) || [''];
-    let lBValue = lBottomInput.val().match(regex) || [''];
-
-    rTValue = rTValue.join('') === '' ? '0' : (Number(rTValue.join('')) > 200 ? '200' : rTValue.join(''));
-    rBValue = rBValue.join('') === '' ? '0' : (Number(rBValue.join('')) > 200 ? '200' : rBValue.join(''));
-    lTValue = lTValue.join('') === '' ? '0' : (Number(lTValue.join('')) > 200 ? '200' : lTValue.join(''));
-    lBValue = lBValue.join('') === '' ? '0' : (Number(lBValue.join('')) > 200 ? '200' : lBValue.join(''));
+    rTValue =
+      rTValue.join("") === ""
+        ? "0"
+        : Number(rTValue.join("")) > 200
+        ? "200"
+        : rTValue.join("");
+    rBValue =
+      rBValue.join("") === ""
+        ? "0"
+        : Number(rBValue.join("")) > 200
+        ? "200"
+        : rBValue.join("");
+    lTValue =
+      lTValue.join("") === ""
+        ? "0"
+        : Number(lTValue.join("")) > 200
+        ? "200"
+        : lTValue.join("");
+    lBValue =
+      lBValue.join("") === ""
+        ? "0"
+        : Number(lBValue.join("")) > 200
+        ? "200"
+        : lBValue.join("");
 
     rTopInput.val(rTValue);
     rBottomInput.val(rBValue);
@@ -47,27 +65,28 @@ $(document).ready(() => {
     const newValues = getValues();
 
     const newBorder = `${newValues.leftTop}px ${newValues.rightTop}px ${newValues.rightBottom}px ${newValues.leftBottom}px`;
-    box.css('border-radius', newBorder);
-    box.css('-moz-border-radius', newBorder);
-    box.css('-webkit-border-radius', newBorder);
+    box.css("border-radius", newBorder);
+    box.css("-moz-border-radius", newBorder);
+    box.css("-webkit-border-radius", newBorder);
 
     const codePreview = `<div>border-radius: ${newBorder};</div>
 <div>-moz-border-radius: ${newBorder};</div>
 <div>-webkit-border-radius: ${newBorder};</div>
-    `
+    `;
 
     preview.html(codePreview);
-    preview.removeClass('hide');
-    copyBtn.removeClass('hide');
+    preview.removeClass("hide");
+    copyBtn.removeClass("hide");
 
     values = newValues;
   }
 
   function copyResults() {
-    const temp = $('<input>');
-    $('body').append(temp);
-    temp.val(preview.text()).select();
-    document.execCommand("copy");
-    temp.remove();
+    const newValues = getValues();
+    const newBorder = `${newValues.leftTop}px ${newValues.rightTop}px ${newValues.rightBottom}px ${newValues.leftBottom}px`;
+
+    navigator.clipboard.writeText(
+      `border-radius: ${newBorder};\n-moz-border-radius: ${newBorder};\n-webkit-border-radius: ${newBorder};`
+    );
   }
 });
